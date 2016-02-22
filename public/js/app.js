30d990f6609ceec44f011ee9c282a40cec845e08
rohanparanjpe <rohanparanjpe555@gmail.com>
2016-02-22 16:50:50 -0600

    addded listner to front end

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+/*!
+   *
+ */
+
+
+var socket = io(); // it is defined in socket.io cdn  
+
+socket.on('connect',function(){
+    
+    console.log('connected to socketIO server');
+    
+    
+});
